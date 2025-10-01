ff46b8396e6f67dd272f4297bf9d1c3c1877888e
RozyShindra <shindrarozy@gmail.com>
2025-10-02 05:18:11 +0530

    added more entity types

--- src/main/java/com/rozynlp/informationextractor/model/Type.java
+++ src/main/java/com/rozynlp/informationextractor/model/Type.java
@@ -4,11 +4,27 @@ public enum Type {
     PERSON("PERSON"),
     LOCATION("LOCATION"),
     ORGANIZATION("ORGANIZATION"),
+    MISC("MISC"),                 // Miscellaneous
+    DATE("DATE"),
+    TIME("TIME"),
+    DURATION("DURATION"),
+    SET("SET"),                   // Collections like "group of people"
+    MONEY("MONEY"),
+    PERCENT("PERCENT"),
     EMAIL("EMAIL"),
+    URL("URL"),
+    PHONE("PHONE"),
     TITLE("TITLE"),
     CITY("CITY"),
     COUNTRY("COUNTRY"),
-    STATE_OR_PROVINCE("STATE_OR_PROVINCE");
+    STATE_OR_PROVINCE("STATE_OR_PROVINCE"),
+    NATIONALITY("NATIONALITY"),
+    RELIGION("RELIGION"),
+    LANGUAGE("LANGUAGE"),
+    EVENT("EVENT"),
+    WORK_OF_ART("WORK_OF_ART"),
+    LAW("LAW"),
+    FACILITY("FACILITY");
 
     private final String type;
 
